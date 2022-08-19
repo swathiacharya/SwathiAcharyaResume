c377c90ba4170f9a1502cda12d47a6ab28128ab4
swathiacharya <acharyaswathi999@gmail.com>
2022-08-19 09:36:12 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import {AccordionModule} from 'primeng/accordion'; 
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {TabViewModule} from 'primeng/tabview';
+import {CardModule} from 'primeng/card';
+import {GalleriaModule} from 'primeng/galleria';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    // AccordionModule, 
+    GalleriaModule,
+    TabViewModule,
+    CardModule,
+    // AccordionModule
+    AccordionModule,
+    BrowserAnimationsModule,
+
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
